b14c39c0ef13c2f4c6179e943d7bd24bdbadf131
unknown <jparham@afrotc-01.argo.uwf.edu>
2013-02-27 16:52:27 -0600

    Added method for updating the main form table

--- src/cardreader/ui/Controller.java
+++ src/cardreader/ui/Controller.java
@@ -9,6 +9,7 @@ import cardreader.dialog.ReportDialog;
 import commonutilities.swing.ComponentPosition;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFileChooser;
@@ -60,14 +61,14 @@ public class Controller {
         dialog.setVisible(true);
     }
 
-    public void fileSaveAs() {
-        JOptionPane.showMessageDialog(null, "Replace XXXXX in file name with appropriate event (PT, LLAB, AS100, AS200, AS300 or AS400)", "Update Name", JOptionPane.PLAIN_MESSAGE);
+    public void fileSaveAs(String eventName) {
+//        JOptionPane.showMessageDialog(null, "Replace XXXXX in file name with appropriate event (PT, LLAB, AS100, AS200, AS300 or AS400)", "Update Name", JOptionPane.PLAIN_MESSAGE);
         JFileChooser chooser = new JFileChooser();
         chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
         FileNameExtensionFilter filter = new FileNameExtensionFilter(
         "Text Files", "txt");
         chooser.setFileFilter(filter);
-        chooser.setSelectedFile(new File(mParser.gregorianDateTime() + "_XXXXX_Attendance.txt"));
+        chooser.setSelectedFile(new File(mParser.gregorianDateTime() + "_" + eventName + "_Attendance.txt"));
         if (chooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION){
             try {
                 FileUtils.copyFile(new File(mModel.getPreferences().getFileLocation() + "/attendanceData.txt"), chooser.getSelectedFile());
@@ -77,10 +78,44 @@ public class Controller {
         }
     }
 
-    void runReport(EventTypes type) {
+    public void runReport(EventTypes type) {
         ReportDialog dialog = new ReportDialog(null,true,type, mModel);
         ComponentPosition.centerFrame(dialog);
         dialog.setVisible(true);
+    }
+
+    public void updateDataTable(String eventName) {
+        mModel.setSelectedEvent(eventName);
+        EventTypes eventType = null;
+        if (eventName.equals("PT")){
+            eventType = EventTypes.PT;
+        } else if (eventName.equals("LLAB")){
+            eventType = EventTypes.LLAB;            
+        } else if (eventName.equals("AS100")){
+            eventType = EventTypes.AS100;            
+        } else if (eventName.equals("AS200")){
+            eventType = EventTypes.AS200;            
+        } else if (eventName.equals("AS300")){
+            eventType = EventTypes.AS300;            
+        } else if (eventName.equals("AS400")){
+            eventType = EventTypes.AS400;            
+        }
+        List<Integer> expectedIDs = null;
+        if (eventType != EventTypes.PT && eventType != EventTypes.LLAB){
+            expectedIDs = mModel.getPersonnelData().getIDsForAsLevel(eventType);
+        } else {
+            expectedIDs = mModel.getPersonnelData().getAllIDs();
+        }
+        List<Integer> idsInDataFile = mModel.getPersonnelData().getIDsInDataFile();
+
+        mModel.getDataTableModel().clearData();
+        for (int expected : expectedIDs) {
+            if (!idsInDataFile.contains(expected)){
+                mModel.getDataTableModel().addName(mModel.getPersonnelData().getName(expected));
+            }
+        }
+        mModel.forceUpdate();
+//        mModel.getDataTableModel().fireTableDataChanged();
     }
 
 }
